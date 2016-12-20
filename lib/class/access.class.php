8b8e6a294fa49cd3216bfece3c96cd195fbc92fc
Michael A. MacDonald <mars@antlersoft.com>
2016-12-20 11:31:36 -0500

    Fix bug logging source host

--- lib/class/access.class.php
+++ lib/class/access.class.php
@@ -252,7 +252,7 @@ class Access {
         }
 
         // Clean incoming variables
-		debug_event('check_network', "ip = $ip + server = ".$_SERVER['REMOTE_ADDR']);
+		debug_event('check_network', "ip = $ip + server = ".$_SERVER['REMOTE_ADDR'], 3);
         $ip = $ip ?: $_SERVER['REMOTE_ADDR'];
 		if ($ip == '::1') {
 		    $ip = '127.0.0.1';
